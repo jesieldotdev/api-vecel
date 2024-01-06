d0db5ba69ad22ce94b49227fe89f7425307e4f67
jesieldotdev <jesiel364@gmail.com>
2024-01-06 19:24:03 -0400

    vff

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,32 @@
 import express from 'express'
-import { prismaClient } from './database'
+import {randomUUID} from "node:crypto"
 
 const app = express()
 app.use(express.json())
 
 const port = process.env.PORT ?? 4000
 
+const books = [{
+    id: randomUUID(),
+    title: "Alice",
+    description: ""
+  }]
+
 app.get('/books', async (request, response) => {
-  const books = await prismaClient.book.findMany()
+  // const books = await prismaClient.book.findMany()
+  
   return response.json(books)
 })
 
 app.post('/books', async (request, response) => {
-  const { description, name } = request.body
-  const book = await prismaClient.book.create({
-    data: {
+  const { description, title } = request.body
+  const book = {
+    id:  randomUUID(),
       description,
-      name,
-    },
-  })
+      title,
+
+  }
+  books.push(book)
   return response.json(book)
 })
 
