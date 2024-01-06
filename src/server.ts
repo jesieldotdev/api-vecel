f401baa7810d8f2726cae11b208f514b195efb92
jesieldotdev <jesiel364@gmail.com>
2024-01-06 19:50:25 -0400

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import {randomUUID} from "node:crypto"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const port = process.env.PORT ?? 4000
 
